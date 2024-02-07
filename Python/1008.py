59fc4677b9c50850744b1d3eceafb7362f2ec91e
ThiagoElias <contatothiagoelias22@gmail.com>
2024-02-06 22:18:26 -0300

    Daily python beecrowd Grind

---
+++ Python/1008.py
@@ -0,0 +1,6 @@
+numb = int(input());
+hours = int(input());
+salperhour = float(input());
+salary = hours * salperhour;
+print('NUMBER =', numb);
+print('SALARY = U$ %.2f' %salary);
---
+++ Python/1009.py
@@ -0,0 +1,5 @@
+name = str(input());
+fixsal = float(input());
+allsells = float(input()) * 0.15;
+res = fixsal + allsells;
+print('TOTAL = R$ %.2f' %res);
